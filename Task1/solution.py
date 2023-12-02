b4321820fe3a6b6efbf4b7421f18dc198d620c89
Neyromant <Neyromant777@gmail.com>
2023-12-02 17:14:44 +0300

    765556

--- Task1/solution.py
+++ Task1/solution.py
@@ -1,31 +1,43 @@
-#https://adventofcode.com/2023/day/1
-list_str = [i.strip("\n ") for i in open("data.txt")]
-def convert_str(str_in):
-    str_out = ""
-    dict_convert = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, }
-    for key in dict_convert:
-        count_key = str_in.count(key)
-        key_1, key_2 = key[1:], key[:-1]
-        if key in str_in:
-            if str_out:
-                count_key_out = str_out.count(key)
-                if count_key_out == count_key:
-                    str_out = str_out.replace(key, str(dict_convert[key]))
-                else:
-                    if count_key == str_out.count(key_1):
-                        str_out = str_out.replace(key_1, str(dict_convert[key]))
-                    elif count_key == str_out.count(key_2):
-                        str_out = str_out.replace(key_2, str(dict_convert[key]))
+#https://adventofcode.com/2023/day/2
+dict_data_game = {}
+for i in open("data.txt"):
+    str_ = i.strip("\n ")
+    index_game = str_.split(":")[0].split()[1]
+    dict_data_game.update({index_game: {}})
+    list_game = str_.split(":")[1:][0].replace(";", ",").split(",")
+    for j in list_game:
+        list_j = j.split()
+        key_1, key_2 = list_j[0], list_j[1]
+        if key_1.isdigit():
+            if dict_data_game[index_game].get(key_2):
+                dict_data_game[index_game][key_2].append(int(key_1))
             else:
-                str_out = str_in.replace(key, str(dict_convert[key]))
-    return str_out if str_out else str_in
-list_str_convert = [convert_str(i) for i in list_str]
-def get_sum(list_data):
-    sum_all = 0
-    for i in list_data:
-        list_num_ = [j for j in i if j.isdigit()]
-        if list_num_:
-            sum_all += int(list_num_[0] + list_num_[-1])
-    return sum_all
-print(f"Решение задания 1: {get_sum(list_str)}")
-print(f"Решение задания 2: {get_sum(list_str_convert)}")
+                dict_data_game[index_game].update({key_2: [int(key_1)]})
+        else:
+            if dict_data_game[index_game].get(key_1):
+                dict_data_game[index_game][key_1].append(int(key_2))
+            else:
+                dict_data_game[index_game].update({key_1: [int(key_2)]})
+dict_rule = {"red":12, 'green':13, 'blue':14}
+set_impossible_game = set()
+all_summ_game = 0
+for game, data_game in dict_data_game.items():
+    game = int(game)
+    all_summ_game += game
+    for key, count in dict_rule.items():
+        data_game_for_key = data_game.get(key)
+        if data_game_for_key:
+            if max(data_game_for_key) > count:
+                set_impossible_game.add(game)
+                break
+power = 0
+for game, data_game in dict_data_game.items():
+    list_max = []
+    for key, list_count in data_game.items():
+        list_max.append(max(list_count))
+    power_game = 1
+    for i in list_max:
+        power_game *=i
+    power +=power_game
+print(f"Решение задания 1: {all_summ_game - sum(set_impossible_game)}")
+print(f"Решение задания 2: {power}")
