f8ef36e5d059550c3e23cf56b3d2a2b9c00dcb96
Neyromant <Neyromant777@gmail.com>
2023-12-02 12:16:33 +0300

    765556

--- Task2/solution.py
+++ Task2/solution.py
@@ -1,26 +1,27 @@
-dict_data = {}
+#https://adventofcode.com/2023/day/2
+dict_data_game = {}
 for i in open("data.txt"):
     str_ = i.strip("\n ")
     index_game = str_.split(":")[0].split()[1]
-    dict_data.update({index_game: {}})
+    dict_data_game.update({index_game: {}})
     list_game = str_.split(":")[1:][0].replace(";", ",").split(",")
     for j in list_game:
         list_j = j.split()
         key_1, key_2 = list_j[0], list_j[1]
         if key_1.isdigit():
-            if dict_data[index_game].get(key_2):
-                dict_data[index_game][key_2].append(int(key_1))
+            if dict_data_game[index_game].get(key_2):
+                dict_data_game[index_game][key_2].append(int(key_1))
             else:
-                dict_data[index_game].update({key_2: [int(key_1)]})
+                dict_data_game[index_game].update({key_2: [int(key_1)]})
         else:
-            if dict_data[index_game].get(key_1):
-                dict_data[index_game][key_1].append(int(key_2))
+            if dict_data_game[index_game].get(key_1):
+                dict_data_game[index_game][key_1].append(int(key_2))
             else:
-                dict_data[index_game].update({key_1: [int(key_2)]})
+                dict_data_game[index_game].update({key_1: [int(key_2)]})
 dict_rule = {"red":12, 'green':13, 'blue':14}
 set_not_impossible_game = set()
 all_summ_game = 0
-for game, data_game in dict_data.items():
+for game, data_game in dict_data_game.items():
     game = int(game)
     all_summ_game += game
     for key, count in dict_rule.items():
@@ -30,7 +31,7 @@ for game, data_game in dict_data.items():
                 set_not_impossible_game.add(game)
                 break
 power = 0
-for game, data_game in dict_data.items():
+for game, data_game in dict_data_game.items():
     list_max = []
     for key, list_count in data_game.items():
         list_max.append(max(list_count))
