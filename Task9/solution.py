b28435982aed3d9135ff3f57198a7c98f59fbf46
Neyromant <Neyromant777@gmail.com>
2023-12-12 00:40:45 +0300

    312

--- Task9/solution.py
+++ Task9/solution.py
@@ -1,3 +1,4 @@
+#https://adventofcode.com/2023/day/9
 list_str = [i.strip("\n ").split() for i in open("input.txt")]
 list_num = [[int(j) for j in i] for i in list_str]
 def get_sum(list_start):
@@ -9,5 +10,6 @@ def get_sum(list_start):
         list_start = [list_start[idx+1] - i for idx, i in enumerate(list_start) if idx < len_l]
         summ_all += list_start[-1]
     return summ_all
+import time
 print(f"Решение задания 1:{sum([get_sum(i) for i in list_num])}")
 print(f"Решение задания 2:{sum([get_sum(i[::-1]) for i in list_num])}")
